fd75d072fd31567e9eb1a81da7939e17d0c88bc6
Rishabh Gour <rishabhgour62@gmail.com>
2018-04-30 01:41:51 +0530

    basic HTTP verbs implemented

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import routes from "./src/routes/crmRoute";
 
 const app = express();
 const PORT = 3000;
 
+routes(app);
+
 app.get("/", (req, res) => {
-   return res.send(`Node and Express server is running on ${PORT}`)
+   return res.send(`Node and Express server is running on ${PORT}`);
 });
 
 app.listen(PORT, ()=>console.log(`server started on port ${PORT}`));
